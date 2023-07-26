d70a3cb1828c48b8146ea9b101b8b6080b9681d9
Joseph Teklu <jteklu@outlook.com>
2023-07-25 21:44:29 -0600

    finished shapes.js, index.js, and examples directory

--- index.js
+++ index.js
@@ -1,27 +1,34 @@
 // installing packages for use
 const inquirer = require("inquirer");
-const fs = require("fs");
+const createSVG = require("./lib/shapes");
 
 // list of question about image for user
 let questions = 
 [
     {type: "input", message: "Provide the three letters you would like in the image", name: "letters", default: "img"},
-    {type: "input", message: "Provide a text color in either words or 6 digit hexadecimal format", name: "textColor", default: "red"},
+    {type: "input", message: "Provide a text color in either words ex:[white, black, pink, purple, green] or 6 digit hexadecimal format", name: "lettersColor", default: "red"},
     {type: "list", message: "Pick the shape you desire", choices: ["Circle", "Square", "Triangle"], name:"shape"},
-    {type: "input", message:"Provide a shape color in either words or 6 digit hexadecimal format", name: "shapeColor"}
+    {type: "input", message:"Provide a shape color in either words or 6 digit hexadecimal format", name: "shapeColor", default: "black"},
+    {type: "input", message: "Provide the file name for the svg", name: "fileName"}
 ];
 
 inquirer.prompt(questions)
 .then((response) => {
-    // if the choosen letters are not 3 show error message
-    if(response.letters.length !== 3){
-        console.info("\nThe letters you have chosen are longer than 3 characters, it has been changed to (img) for you.\n");
-    } 
-    // if the user gave a hexadeimal that is not the right format show error
-    if(!/^#[0-9A-F]{6}$/i.test(response.textColor)){
-        console.info("\nThe text color hexadecimal you have entered is not valid. It has been changed to RED for you.\n")
-    }
-    if (response.testColor !== "red") {
-        
-    }
+
+    let shape;
+    let svg;
+
+    // based on the choosen shape set new instance with the given shape color
+    if(response.shape == "Circle"){shape = new createSVG.Circle(response.shapeColor);}
+    if(response.shape == "Square"){shape = new createSVG.Square(response.shapeColor);}
+    if(response.shape == "Triangle"){shape = new createSVG.Triangle(response.shapeColor);}
+
+    // call the SVG class and create instance from the data given and the shape instance created with the shape color
+    svg = new createSVG.SVG(response.letters, response.lettersColor, shape, response.fileName)
+
+    // return the svg
+    return svg;
+}).then(svg => {
+    // call the write to file function to setup the shape
+    svg.writeToFile();
 })
